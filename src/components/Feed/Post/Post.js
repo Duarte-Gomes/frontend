4529c6b55a9af1db306566d56ebb07229e47deb5
Duarte <animal.dg@gmail.com>
2024-11-28 12:23:08 +0000

    foto a funcionar no inicio

--- src/components/Feed/Post/Post.js
+++ src/components/Feed/Post/Post.js
@@ -1,21 +1,25 @@
 import React from 'react';
+import Image from '../../../components/Image/Image';
 
 import Button from '../../Button/Button';
 import './Post.css';
 
 function post(props) {
+    const teste = "http://localhost:8080/"+ props.image;
+
+    
     return (
         <article className="post">
             <header className="post__header">
                 <h3 className="post__meta">
-                    Posted by {props.author} on {props.date}
+                    Categoria: {props.category}
                 </h3>
                 <h1 className="post__title">{props.title}</h1>
             </header>
-            {/* <div className="post__image">
-              <Image imageUrl={props.image} contain />
+            <div className="post__image">
+              <Image imageUrl= {teste} contain />
             </div>
-            <div className="post__content">{props.content}</div> */}
+            {/* <div className="post__content">{props.content}</div> */}
             <div className="post__actions">
                 <Button mode="flat" link={props.id}>
                     View
